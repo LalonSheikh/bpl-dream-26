a974d3ac99856032f3e08c0d389f5c80a8fcb95c
LalonSheikh <abusalekmdlalonsheikh@gmail.com>
2026-04-01 23:15:28 +0600

    first commit

--- src/components/players/Players.jsx
+++ src/components/players/Players.jsx
@@ -6,8 +6,14 @@ const Players = ({ playerPromise }) => {
   const players = use(playerPromise);
   console.log(players);
   return (
-    <div className="container mx-auto">
-      Players: {players.length}
+    <div className="container mx-auto my-[60px]">
+      <div className="flex justify-between items-center gap-4 mb-[20px]">
+        <h2 className="font-bold text-3xl">Available Players</h2>
+        <div>
+          <button className="btn bg-[#e7fe29] rounded-r-none rounded-l-xl">Available</button>
+          <button className="btn btn-neutral rounded-l-none rounded-r-xl">Selected</button>
+        </div>
+      </div>
       <AvailablePlayers players={players}></AvailablePlayers>
     </div>
   );
